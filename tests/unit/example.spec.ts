28daa82119f02fde873cae447bfd93e5bfec6753
aruan <Aruan123>
2022-01-03 01:12:11 +0600

    init

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -3,7 +3,7 @@ import HelloWorld from "@/components/HelloWorld.vue";
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
-    const msg = "new letter";
+    const msg = "new letter 1";
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg },
     });
