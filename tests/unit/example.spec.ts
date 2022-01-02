40ed00f84099a1bf5b60aab27bf3a1f774d05b1e
aruan <Aruan123>
2022-01-03 01:40:48 +0600

    rename test

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -3,7 +3,7 @@ import HelloWorld from "@/components/HelloWorld.vue";
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
-    const msg = "vuetify-loa";
+    const msg = "vuetify-loader";
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg },
     });
